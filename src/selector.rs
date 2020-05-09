b80f41ba21137a31043b0d90cd67842c909891b7
hibi221b <hibi221b@hibi221b-2.local>
2020-05-10 00:22:22 +0900

    update src/selector.rs

--- src/selector.rs
+++ src/selector.rs
@@ -57,61 +57,61 @@ pub const HIT_REPOSITORY_SELECTOR           : &'static str = "#js-pjax-container
 pub const LENGTH_OF_ELEMENTS                : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li";
 
 //1                                                                                                                                                                     //li:nth-child(1~10)
-pub const REPO_NAME_AND_HREF_SELECTOR_1     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_1                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > p";
-pub const STAR_SELECTOR_1                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_1               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_1     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_1                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > p";
+const STAR_SELECTOR_1                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_1               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(1) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //2
-pub const REPO_NAME_AND_HREF_SELECTOR_2     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_2                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > p";
-pub const STAR_SELECTOR_2                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_2               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_2     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_2                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > p";
+const STAR_SELECTOR_2                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_2               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(2) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //3
-pub const REPO_NAME_AND_HREF_SELECTOR_3     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_3                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > p";
-pub const STAR_SELECTOR_3                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_3               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_3     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_3                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > p";
+const STAR_SELECTOR_3                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_3               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(3) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //4
-pub const REPO_NAME_AND_HREF_SELECTOR_4     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_4                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > p";
-pub const STAR_SELECTOR_4                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_4               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_4     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_4                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > p";
+const STAR_SELECTOR_4                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_4               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(4) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //5
-pub const REPO_NAME_AND_HREF_SELECTOR_5     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_5                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > p";
-pub const STAR_SELECTOR_5                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_5               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_5     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_5                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > p";
+const STAR_SELECTOR_5                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_5               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(5) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //6
-pub const REPO_NAME_AND_HREF_SELECTOR_6     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_6                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > p";
-pub const STAR_SELECTOR_6                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_6               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_6     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_6                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > p";
+const STAR_SELECTOR_6                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_6               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(6) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //7
-pub const REPO_NAME_AND_HREF_SELECTOR_7     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_7                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > p";
-pub const STAR_SELECTOR_7                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_7               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_7     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_7                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > p";
+const STAR_SELECTOR_7                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_7               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(7) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //8
-pub const REPO_NAME_AND_HREF_SELECTOR_8     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_8                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > p";
-pub const STAR_SELECTOR_8                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_8               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_8     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_8                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > p";
+const STAR_SELECTOR_8                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_8               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(8) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //9
-pub const REPO_NAME_AND_HREF_SELECTOR_9     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_9                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > p";
-pub const STAR_SELECTOR_9                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_9               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_9     : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_9                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > p";
+const STAR_SELECTOR_9                   : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_9               : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(9) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
 
 //10
-pub const REPO_NAME_AND_HREF_SELECTOR_10    : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > div.f4.text-normal > a";
-pub const DESC_SELECTOR_10                  : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > p";
-pub const STAR_SELECTOR_10                  : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
-pub const KEYWORDS_SELECTOR_10              : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
+const REPO_NAME_AND_HREF_SELECTOR_10    : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > div.f4.text-normal > a";
+const DESC_SELECTOR_10                  : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > p";
+const STAR_SELECTOR_10                  : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > div:nth-child(3) > div.d-flex.flex-wrap.text-small.text-gray > div:nth-child(1) > a";
+const KEYWORDS_SELECTOR_10              : &'static str = "#js-pjax-container > div > div.col-12.col-md-9.float-left.px-2.pt-3.pt-md-0.codesearch-results > div > ul > li:nth-child(10) > div.mt-n1 > div:nth-child(3) > div:nth-child(1)";
